39e5050c33fad2346c703263ef4212260565285d
fenceFoil <fenceFoil@mailinator.com>
2016-01-30 14:01:43 -0500

    Commit time!

--- GGJ2016/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ GGJ2016/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,14 +7,6 @@ import com.mygdx.game.CoreGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		
-		config.title = "Dungeon Crawler 2.0";
-//		config.useGL30 = true;
-		config.width = 1024;
-		config.height = 800;
-		
 		new LwjglApplication(new CoreGame(), config);
 	}
-	
-	
 }
