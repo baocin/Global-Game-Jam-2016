00b0be4c1c62cc348e94ca20d9a65bd63cace85f
Michael Pedersen <steele.pedersen@gmail.com>
2016-01-30 13:20:42 -0500

    Bare bones game

--- GGJ2016/core/src/com/mygdx/game/CoreGame.java
+++ GGJ2016/core/src/com/mygdx/game/CoreGame.java
@@ -1,27 +1,69 @@
 package com.mygdx.game;
 
+import java.util.HashMap;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CoreGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+	private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
+    private CreditScreen creditScreen;
+    private GameScreen gameScreen;
+    private PauseScreen pauseScreen;
+    private TitleScreen titleScreen;
+    
+    @Override
+    public void create() {
+    	creditScreen = new CreditScreen();
+    	gameScreen= new GameScreen();
+    	pauseScreen = new PauseScreen();
+    	titleScreen = new TitleScreen();
+    	
+        batch = new SpriteBatch();
+        texture = new Texture(Gdx.files.internal("perspective.png"));
+        sprite = new Sprite(texture);
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+    }
+
+    @Override
+    public void render() {
+    	if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+    		sprite.translateY(10.0f);
+        }
+    	if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+    		sprite.translateY(-10.0f);
+        }
+    	
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
 }
