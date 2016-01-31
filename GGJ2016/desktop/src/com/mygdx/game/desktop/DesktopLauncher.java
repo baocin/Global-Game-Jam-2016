ab6870d4f6b8197ae7d7e7e1ef2a2f7c6aaf9508
Michael Pedersen <steele.pedersen@gmail.com>
2016-01-30 22:23:56 -0500

    Had to remake libGDX project. Going to rebase.

--- GGJ2016/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ GGJ2016/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,15 +1,12 @@
 package com.mygdx.game.desktop;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.utils.Logger;
-import com.teamsomething.ggj2016.game.CoreGame;
-import com.teamsomething.ggj2016.game.GameScreenTest;
+import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GameScreenTest(), config);
+		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
